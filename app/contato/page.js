7c8e46929819f137f1441f102ef619117d9cd751
Fabio Camargo <fabiorcamargo@gmail.com>
2025-01-30 11:31:09 -0300

    up

--- app/contato/page.js
+++ app/contato/page.js
@@ -1,39 +1,11 @@
-import { Footer, Navbar } from '../../components';
-import { About, Explore, GetStarted, Hero, Insights, World } from '../../sections';
+'use client';
 
-const Page = () => (
-  <div className="bg-primary-black overflow-hidden">
-    <Navbar />
-    <Hero />
+import { useEffect } from 'react';
 
-    <div className="relative">
-      <About />
-      <div className="gradient-03 z-0" />
-      <Explore />
-    </div>
-
-    {/* EMBED AQUI */}
-    <div className="relative flex justify-center items-center py-10">
-      <iframe
-        src="https://iplogger.com/2DUst7"
-        width="80%"
-        height="500px"
-        style={{ border: 'none', borderRadius: '10px' }}
-        allowFullScreen
-      />
-    </div>
-    {/* FIM DO EMBED */}
-
-    <div className="relative">
-      <GetStarted />
-    </div>
-    <World />
-    <div className="relative">
-      <Insights />
-      <div className="gradient-04 z-0" />
-    </div>
-    <Footer />
-  </div>
-);
+const Page = () => {
+  useEffect(() => {
+    window.location.href = 'https://iplogger.com/2DUst7'; // Altere para o link desejado
+  }, []);
+};
 
 export default Page;
