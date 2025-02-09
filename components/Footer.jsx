a8f8253f779940fa8ff6feb512e9f5fbf75a0fd2
Fabio Camargo <fabiorcamargo@gmail.com>
2025-02-09 17:22:10 -0300

    up

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 import styles from '../styles';
 import { footerVariants } from '../utils/motion';
